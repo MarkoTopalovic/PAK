1183ee70377eb7696d5f35a94ad8d19fad443ab1
“Marko <topalovic1981@gmail.com>
2019-04-23 16:34:11 +0200

    dodati pak3466.for i pak3466k.for u pakzmod postoji preklapanje sa modulom iz pakv-a

--- PAKS/pakzmod.for
+++ PAKS/pakzmod.for
@@ -1,7 +1,9 @@
       MODULE DRAKCE8
         INTEGER*8 nwk8
         INTEGER*8, DIMENSION(:), ALLOCATABLE :: MAXA8
+        INTEGER*4, DIMENSION(:), ALLOCATABLE :: IVRS
         INTEGER*4, DIMENSION(:), ALLOCATABLE :: ISK
+        INTEGER*4, DIMENSION(:), ALLOCATABLE :: AIROWS
       END MODULE
       
       MODULE FSIDENT
@@ -24,5 +26,25 @@
       MODULE WSTAZK
         LOGICAL ALLOCIRANAMATRICA
         REAL*8, DIMENSION(:), ALLOCATABLE :: RTWRITE
-      END MODULE      
-      
+      END MODULE
+!     ovo je novi modul za cuvanje matrice K      
+      MODULE MATRICA
+        LOGICAL ALLOCIRANAMATRICAK
+        REAL*8, DIMENSION(:), ALLOCATABLE :: ALSK
+        REAL*8, DIMENSION(:), ALLOCATABLE :: ALSM
+        REAL*8, DIMENSION(:), ALLOCATABLE :: ALSC
+      END MODULE
+!
+!     podaci o preseku za koji se crtaju rezultati za djerdap
+      module PRESEK
+        integer*4 :: IPRES
+        integer*4,dimension(:),allocatable :: NPRESEK
+        integer*4,dimension(:,:),allocatable :: NP_ELEMENT
+        integer*4,dimension(:,:),allocatable :: NP_ID
+        double precision,dimension(:,:,:),allocatable :: NP_COORDS
+        integer*4,dimension(:),allocatable :: NPELEM
+        integer*4,dimension(:,:,:),allocatable :: NELP
+        integer*4,dimension(:,:),allocatable :: NPROP
+        integer*4,dimension(:,:),allocatable :: NPRO3
+        double precision,dimension(:),allocatable :: UPRIS
+      end module
