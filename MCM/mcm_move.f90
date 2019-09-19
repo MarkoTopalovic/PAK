f285a158e4ee53d28f67e8ada54543c1bc04b689
“Marko <topalovic1981@gmail.com>
2019-09-19 14:18:22 +0200

    neonka

--- MCM/mcm_move.f90
+++ MCM/mcm_move.f90
@@ -53,14 +53,16 @@ endif
 !  passed through an active symmetry plane
 !
 if(mcm_boundary) call mcm_check_sym_pen
-!
+
 if(mcm_emitter.eq.(.true.))then ! hardcoded
     icounter = 0
  do i=1,mcm_np
-      if((par(i)%x(2).le.200).and.(par(i)%mat.eq.2).and.(par(i)%newborn.eq.(.true.))) then
+      !if((par(i)%x(2).le.200).and.(par(i)%mat.eq.2).and.(par(i)%newborn.eq.(.true.))) then !casa v.1.0
+     if((par(i)%x(2).le.0.01).and.(par(i)%x(2).gt.0).and.(par(i)%newborn.eq.(.true.))) then !casa v.2.0
           icounter = icounter + 1
           par(mcm_np + icounter)=par(i)
-          par(mcm_np + icounter)%x(2) = par(mcm_np + icounter)%x(2) +1
+          !par(mcm_np + icounter)%x(2) = par(mcm_np + icounter)%x(2) +1 !casa v.1.0
+          par(mcm_np + icounter)%x(2) = par(mcm_np + icounter)%x(2) +0.001
           !par(mcm_np + icounter)%x(1) = par(mcm_np + icounter)%x(1) +1
        par(i)%newborn = .false.
       endif
